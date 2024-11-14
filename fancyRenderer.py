a904992b022a20fdf27c2a8d9d2a6a75adf17d9a
Nate McKenzie <a02298217@usu.edu>
2024-11-13 21:34:42 -0700

    Hide the cursor while rendering.

--- fancyRenderer.py
+++ fancyRenderer.py
@@ -5,14 +5,13 @@ import curses
 class FancyRenderer:
     def __init__(self):
         self.window = curses.initscr()
+        curses.curs_set(0) # 0=invisible
 
     def render(self, simulator: Simulator):
-        debug = open("debug", "w")
         self.window.clear()
         for row in range(simulator.height):
             for col in range(simulator.width):
                 char = simulator.getCell(col, row)
                 self.window.addch(row, col, " " if char is CellState.DEAD else "o")
-        debug.close()
         self.window.refresh()
-        curses.napms(100)
+        curses.napms(300)
