5038b366cfda98307429bb4381e95067d24f3910
Nate McKenzie <a02298217@usu.edu>
2024-12-13 15:54:14 -0700

    Fix diamonds and extract_times.sh

--- extract_times.sh
+++ extract_times.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tail -n 2 generated/*/log.txt | sed "s/==> generated\/\(.\+\)\/.\+/\1,/" | sed "s/.\+ \([0-9]\+\) .\+/\1,/" | sed 'N;s/\n/ /' | sed 'N;s/\n/ /'
+tail -n 2 generated/*/log.txt | sed "s/==> generated\/\(.\+\)\/.\+/\1,/" | sed "s/.\+ \([0-9]\+\) .\+/\1,/" | sed 'N;s/\n/ /' | sed 'N;s/\n/ /' | sed 's/\(.\+\), /\1/'
 #tail -n 2 generated/*/log.txt | sed "s/.\+ \([0-9]\+\) .\+/\1/" | sed 'N;N;/^$/d;s/\n/ /g;P;D'
